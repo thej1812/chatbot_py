de67cb6461647242af19d8f135486e05aa334ab5
thej1812 <thejashwini1800@gmail.com>
2025-03-20 21:46:39 +0530

    app

---
+++ faq_chatbot/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify
+from chatbot import get_response
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/ask", methods=["POST"])
+def ask():
+    user_input = request.json.get("message")
+    response = get_response(user_input)
+    return jsonify({"response": response})
+
+if __name__ == "__main__":
+    app.run(debug=True)
