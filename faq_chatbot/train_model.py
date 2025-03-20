61bd5a9f8b06c78be12d34fdb573054814557484
thej1812 <thejashwini1800@gmail.com>
2025-03-20 21:47:09 +0530

    train model

---
+++ faq_chatbot/train_model.py
@@ -0,0 +1,72 @@
+import json
+import numpy as np
+import random
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import SGD
+import pickle
+
+lemmatizer = WordNetLemmatizer()
+
+# Load intents file
+with open("intents.json", "r") as file:
+    intents = json.load(file)
+
+words = []
+classes = []
+documents = []
+ignore_words = ["?", "!", "."]
+
+for intent in intents["intents"]:
+    for pattern in intent["patterns"]:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent["tag"]))
+        if intent["tag"] not in classes:
+            classes.append(intent["tag"])
+
+# Lemmatization
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(set(words))
+classes = sorted(set(classes))
+
+pickle.dump(words, open("model/words.pkl", "wb"))
+pickle.dump(classes, open("model/classes.pkl", "wb"))
+
+# Training data
+training = []
+output_empty = [0] * len(classes)
+
+for doc in documents:
+    bag = []
+    word_patterns = [lemmatizer.lemmatize(w.lower()) for w in doc[0]]
+    for w in words:
+        bag.append(1 if w in word_patterns else 0)
+    
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training, dtype=object)
+
+train_x = np.array([item[0] for item in training])
+train_y = np.array([item[1] for item in training])
+
+# Build Model
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation="softmax"))
+
+sgd = SGD(learning_rate=0.01, momentum=0.9, decay=1e-6, nesterov=True)
+model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
+
+model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+model.save("model/chatbot_model.h5")
+
+print("Model trained and saved!")
